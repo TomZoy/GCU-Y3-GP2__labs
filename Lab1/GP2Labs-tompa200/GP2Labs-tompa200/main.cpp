884881a5c28daf701a1daa1fa84e6782bbc87b5b
tomzoy <aa.secrets@gmail.com>
2014-09-23 10:34:49 +0100

    point 3 done

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
@@ -3,3 +3,20 @@
 
 //global variables here
 
+void InitWindow(int width, int height, bool fullscreen)
+
+{
+
+
+}
+
+void CleanUp()
+{}
+
+
+
+//Main Methood entry point
+int main(int argc, char * arg[]){
+	return 0;
+
+}

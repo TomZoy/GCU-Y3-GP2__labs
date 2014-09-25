5a5028e1f3f7cc472ce64e2b94454a47edc78299
TomZoy <TomZoy@users.noreply.github.com>
2014-09-25 19:02:33 +0100

    point 5 DONE

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
@@ -1,22 +1,52 @@
 //headerfiles
-#include <iostream>;
+#include <iostream>; //include for input/output handling
+#include <SDL.h>; //header for SDL functionality
 
 //global variables here
+SDL_Window * window;
+const int Window_Width = 640; //constant to control window creation
+const int Window_Height = 480;
+
+
+
 
 void InitWindow(int width, int height, bool fullscreen)
 
 {
+	window = SDL_CreateWindow(
 
+		"Lab 1", //window title
+		SDL_WINDOWPOS_CENTERED, //w cetered
+		SDL_WINDOWPOS_CENTERED, //h centered
+		width, // width, in pixels
+		height, // height, in pixels
+		SDL_WINDOW_OPENGL //flags
+
+		);
 
 }
 
 void CleanUp()
-{}
+{
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
 
 
 
 //Main Methood entry point
 int main(int argc, char * arg[]){
+
+	//init everything - SDLm if it is nonzero we have a problem
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		std::cout << "ERROR SDL_Init" << SDL_GetError() << std::endl;
+		return -1;
+	}
+
+	InitWindow(Window_Width, Window_Height, false);
+
+
+	CleanUp();
 	return 0;
 
 }

0676ac2894495caa748839d859d0e8d4147f4215
TomZoy <TomZoy@users.noreply.github.com>
2014-09-25 22:07:47 +0100

    point 9 DONE

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
@@ -102,6 +102,78 @@ void setViewport(int width, int height)
 }
 
 
+//function to draw the triangle
+void DrawTriangle()
+{
+	//switch to ModelView
+	glMatrixMode(GL_MODELVIEW);
+	//reset using the identity matrix
+	glLoadIdentity();
+	//translate to -5f on Z axis
+	glTranslatef(0.0f, 0.0f, -5.0f);
+
+	//begin drawing the triangles
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f); //colour of the vertices
+	glVertex3f(0.0f, 1.0f, 0.0f); //top
+	glVertex3f(-1.0f, -1.0f, 0.0f); //bottom left
+	glVertex3f(1.0f, -1.0f, 0.0f); //top
+	glEnd();
+
+	/*
+	
+glMatrixMode(GL_MODELVIEW);
+This switches the matrix mode state to the Model View mode, this mode is used to position our vertices in 3D space. We will often carry out a rotation, translation or scaling after this call.
+
+glLoadIdentity();
+This will push the identity matrix onto the current matrix(in this case ModelView , see above).  The identity matrix is like setting a value to 1(a gross simplification but will do for now).
+
+glTranslatef(0.0f,0.0f,-5.0f);
+Will push a translation matrix  onto the current matrix, in this case it will translate the current matrix -5.0f units.
+
+glBegin(GL_TRIANGLES);
+This begins the drawing process, we pass in the primitive type we are going to draw.
+
+glColor(1.0f,0.0f,0.0f);
+This sets the colour of the vertices where each component is a floating point number(0.0f – 1.0f) and represents red, green and blue.
+
+glVertex3f(1.0f,0.0f,0.0f);
+This specifies a Vertex using an x. y and z positional values.
+
+glEnd();
+This ends the drawing process
+
+	*/
+
+	}
+
+
+
+//function to draw
+void render()
+{
+	//set the clear colour
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//clear the colour and depth-buffer
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+	DrawTriangle();
+
+
+	//requirte to swap the back and front buffer
+	SDL_GL_SwapWindow(window);
+
+}
+
+
+
+
+//function to update game state
+void update()
+{
+
+}
 
 
 //Main Methood entry point
@@ -138,13 +210,10 @@ int main(int argc, char * arg[]){
 		}
 
 
+		update();
+		render();
+
 	}
-
-
-
-
-
-
 
 	CleanUp();
 	return 0;

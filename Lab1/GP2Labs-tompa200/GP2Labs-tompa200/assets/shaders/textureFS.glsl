c71d6f582c559d2aa7d900c01069c8709d07deb8
TomZoy <TomZoy@users.noreply.github.com>
2014-10-21 23:23:22 +0100

    Revert "lab4 4.7 done - faulty"
    This reverts commit dc1b81118e0a7583390746cba9c76d94ec2627f9.

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/assets/shaders/textureFS.glsl
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/assets/shaders/textureFS.glsl
@@ -5,6 +5,13 @@ uniform  sampler2D texture0;
 
 void main()
 {
+	// The above  line  using  the  built‐in  GLSL  function  called  texture,
+	// this function like most GLSL functions are fairly generic and will do operations
+	// dependent  on  what  is  passed  in.  
+	// The above  code  shows  how  you  can  carry  out  a  texture  lookup  into  a  
+	// 2D Texture,  but  similar  syntax  will  be  used  for  1D  and  3D  textures.  For  more  
+	// details  have  a  look  at  the  following  URL https://www.opengl.org/wiki/Sampler_(GLSL)#Basic_texture_access
+
 
 	FragColor  =  texture(texture0,  vertexTexCoordsOut);
 }

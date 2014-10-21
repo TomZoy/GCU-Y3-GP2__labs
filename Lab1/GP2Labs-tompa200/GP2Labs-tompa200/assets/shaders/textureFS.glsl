9a5d5bb734d100ece642d209f26c773af59587aa
TomZoy <TomZoy@users.noreply.github.com>
2014-10-21 20:40:26 +0100

    lab4 2.2 done

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/assets/shaders/textureFS.glsl
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/assets/shaders/textureFS.glsl
@@ -1,6 +1,17 @@
-#version 150
+﻿#version 150
 out vec4 FragColor;
+in  vec2  vertexTexCoordsOut;
+uniform  sampler2D texture0;
+
 void main()
 {
-	FragColor = vec4(1.0f,1.0f,1.0f,0.8f);
+	// The above  line  using  the  built‐in  GLSL  function  called  texture,
+	// this function like most GLSL functions are fairly generic and will do operations
+	// dependent  on  what  is  passed  in.  
+	// The above  code  shows  how  you  can  carry  out  a  texture  lookup  into  a  
+	// 2D Texture,  but  similar  syntax  will  be  used  for  1D  and  3D  textures.  For  more  
+	// details  have  a  look  at  the  following  URL https://www.opengl.org/wiki/Sampler_(GLSL)#Basic_texture_access
+
+
+	FragColor  =  texture(texture0,  vertexTexCoordsOut);
 }

d2efb9524c321988434cb81313612202d63859fa
TomZoy <TomZoy@users.noreply.github.com>
2014-10-21 19:46:31 +0100

    lab 4 1.5 done

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Texture.cpp
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Texture.cpp
@@ -5,5 +5,16 @@
 GLuint  loadTextureFromFile(const  std::string&  filename)
 {
 	GLuint  textureID = 0;
+
+
+	SDL_Surface  *imageSurface = IMG_Load(filename.c_str());
+
+	if (!imageSurface){
+		std::cout << "Can'  Load  image  " << filename << "  " << IMG_GetError();
+		return  textureID;
+	}
+
+
+
 	return	textureID;
 }

3cdd4ded6c3b8007546b062629fdf66085b0fdee
TomZoy <TomZoy@users.noreply.github.com>
2014-10-27 20:06:10 +0000

    Lab 1.4 done

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Texture.cpp
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Texture.cpp
@@ -16,21 +16,61 @@ GLuint  loadTextureFromFile(const  std::string&  filename)
 		return  textureID;
 	}
 
+
+	/*
+	The  first  two  lines  setup  Linear  filtering  on  the  texture,  the  next  two  lines  set  the  
+	texture  addressing  mode to  CLAMP and  the  last  line  generates  Mipmaps  for  the  
+	texture (ref  Texturing  Lecture  in  Week)
+	*/
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+
+	return	textureID;
+}
+
+GLuint loadTextureFromFont(const std::string& fontFilename, int pointSize, const std::string& text)
+{
+	GLuint TextureID = 0;
+
+	TTF_Font*font = TTF_OpenFont(fontFilename.c_str(), pointSize);
+
+	if (!font)
+	{
+		std::cout << "Unable to load font " << fontFilename << TTF_GetError();
+		return TextureID;
+	}
+
+	SDL_Surface*textSurface = TTF_RenderText_Blended(font, text.c_str(), { 255, 255, 255 });
+
+	return TextureID;
+}
+
+GLuint convertSDLSurfaceToGLTexture(SDL_Surface*surface)
+{
+	GLuint textureID = 0;
+
 	/*
 	he  first  line  gets  the  number  of  bytes  per  pixel(https://wiki.libsdl.org/SDL_PixelFormat#bytesperpixel),ű
-	this  determines what  the  surface  contains.  If  the  value  is  4  then  that  means  the  surface  contains  
+	this  determines what  the  surface  contains.  If  the  value  is  4  then  that  means  the  surface  contains
 	red,  green,  blue  and  alpha  channels.  If  the  value  is  3  then  it  means  the  surface  only  contains  red,  green  and  blue  ch
 	*/
 
-	GLint nOfColors = imageSurface->format->BytesPerPixel;
-	
+	GLint nOfColors = surface->format->BytesPerPixel;
+
 	GLenum texture_format = GL_RGB;
 	GLenum internalFormat = GL_RGB8;
 
 
 	if (nOfColors == 4)          //  contains  an  alpha  channel
 	{
-		if (imageSurface->format->Rmask == 0x000000ff)
+		if (surface->format->Rmask == 0x000000ff)
 		{
 			texture_format = GL_RGBA;
 			internalFormat = GL_RGBA8;
@@ -46,7 +86,7 @@ GLuint  loadTextureFromFile(const  std::string&  filename)
 
 	else  if (nOfColors == 3)          //  no alpha  channel
 	{
-		if (imageSurface->format->Rmask == 0x000000ff)
+		if (surface->format->Rmask == 0x000000ff)
 		{
 			texture_format = GL_RGB;
 			internalFormat = GL_RGB8;
@@ -81,44 +121,14 @@ GLuint  loadTextureFromFile(const  std::string&  filename)
 	7. The  format  of  the  pixels  we  are  passing  in(SDL  surface  pixels  in  our  case)
 	8. The  data  type  of  the  pixels(in  this  case  unsigned  bytes  0  -­‐255  for  each  component  of  the  pixel)
 	9. The  actual  pixel  data,  notethat  this  comes  from  the  SDL  Surface
-	These  parameters  for  this  function  are  explained  in  more  detail  at  following  URL  
+	These  parameters  for  this  function  are  explained  in  more  detail  at  following  URL
 	https://www.opengl.org/sdk/docs/man3/xhtml/glTexImage2D.xm
 
 	*/
 
-	glTexImage2D(GL_TEXTURE_2D, 0, texture_format, imageSurface->w, imageSurface->h, 0, texture_format, GL_UNSIGNED_BYTE, imageSurface->pixels);
+	glTexImage2D(GL_TEXTURE_2D, 0, texture_format, surface->w, surface->h, 0, texture_format, GL_UNSIGNED_BYTE, surface->pixels);
 
-	/*
-	The  first  two  lines  setup  Linear  filtering  on  the  texture,  the  next  two  lines  set  the  
-	texture  addressing  mode to  CLAMP and  the  last  line  generates  Mipmaps  for  the  
-	texture (ref  Texturing  Lecture  in  Week)
-	*/
+	SDL_FreeSurface(surface);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	SDL_FreeSurface(imageSurface);
-	return	textureID;
-}
-
-GLuint loadTextureFromFont(const std::string& fontFilename, int pointSize, const std::string& text)
-{
-	GLuint TextureID = 0;
-
-	TTF_Font*font = TTF_OpenFont(fontFilename.c_str(), pointSize);
-
-	if (!font)
-	{
-		std::cout << "Unable to load font " << fontFilename << TTF_GetError();
-		return TextureID;
-	}
-
-	SDL_Surface*textSurface = TTF_RenderText_Blended(font, text.c_str(), { 255, 255, 255 });
-
-	return TextureID;
+	return 0;
 }
--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Texture.h
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Texture.h
@@ -11,5 +11,7 @@ GLuint  loadTextureFromFile(const  std::string&  filename);
 
 GLuint loadTextureFromFont(const std::string& fontFilename, int pointSize, const std::string& text);
 
+GLuint convertSDLSurfaceToGLTexture(SDL_Surface*surface);
+
 
 #endif

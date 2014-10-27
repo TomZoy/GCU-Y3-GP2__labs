e1003c88b4908628ed9354ff47c361e0b9c8a64e
TomZoy <TomZoy@users.noreply.github.com>
2014-10-27 20:18:45 +0000

    Lab5 - 2.2+2.3 done

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/main.cpp
@@ -28,6 +28,7 @@ const std::string ASSET_PATH = "assets";
 #endif
 const std::string SHADER_PATH = "/shaders";
 const std::string TEXTURE_PATH = "/textures";
+const std::string FONT_PATH = "/fonts";
 
 
 

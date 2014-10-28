31e8b834524a821d4a820efc14883423dcd8a54d
TomZoy <aa.secrets@gmail.com>
2014-10-28 10:49:39 +0000

    Lab6 - before 2-destroy

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/GameObject.cpp
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/GameObject.cpp
@@ -1,2 +1,49 @@
 #include "GameObject.h"
 
+GameObject::GameObject()
+{
+
+};
+
+
+GameObject::~GameObject()
+{
+
+};
+
+void GameObject::init()
+{
+	for (auto iter = m_Components.begin(); iter != m_Components.end(); iter++)
+	{
+		(*iter)->init();
+	}
+}
+
+void GameObject::update()
+{
+	for (auto iter = m_Components.begin(); iter != m_Components.end(); iter++)
+	{
+		(*iter)->update();
+	}
+};
+
+
+void GameObject::render()
+{
+	for (auto iter = m_Components.begin(); iter != m_Components.end(); iter++)
+	{
+		(*iter)->render();
+	}
+};
+
+void GameObject::destroy()
+{};
+
+const std::string& getName()
+{};
+
+void setName(const std::string& name)
+{};
+
+void addCompnent(Component*component) // NOT SURE ABOUT THIS ....
+{};
--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/GameObject.h
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/GameObject.h
@@ -2,16 +2,35 @@
 #define GAMEOBJECT_H
 
 #include  <string>
+#include "Component.h"
+#include <vector>
+
+using namespace std;  //to avoid putting std:: in front of every bloody thing ....
 
 class GameObject
 {
 public:
+	GameObject();
+	virtual ~GameObject();
+
+	 void init();
+	 void update();
+	 void render();
+	 void destroy();
+
+	 const std::string& getName();
+	 void setName(const std::string& name);
+	 void addCompnent(Component*component); // NOT SURE ABOUT THIS ....
 
 
 protected:
 
 
 private:
+
+	std::string m_Name;
+	vector<Component*>m_Components;
+
 };
 
 #endif

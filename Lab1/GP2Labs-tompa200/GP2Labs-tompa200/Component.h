cd3273bab010dbf9d69b6392933e07577673f154
TomZoy <aa.secrets@gmail.com>
2014-10-28 10:03:54 +0000

    Lab6 - 1.2 done

--- Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Component.h
+++ Lab1/GP2Labs-tompa200/GP2Labs-tompa200/Component.h
@@ -1,6 +1,29 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+#include  <string>
 
+class Component
+{
+public:
+	Component();
+	virtual ~Component();
+
+	virtual void init();
+	virtual void update();
+	virtual void render();
+	virtual void destroy();
+
+
+	const std::string& getType(); // & and const combined means -> once set, can't be changed!!!
+	void toggleActive();
+	bool isActive();
+
+protected:
+	std::string m_Type;
+	bool m_Active;
+
+private:
+};
 
 #endif
